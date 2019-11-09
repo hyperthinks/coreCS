b49c00045769f686aeaaaeb91505e6fa0e971984
manivannansenthil <39681947+manivannansenthil@users.noreply.github.com>
2019-11-09 17:57:43 -0500

    Add files via upload

---
+++ component/login/login.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import loginImg from "../../login.svg"
+
+export class login extends React.Component {
+
+constructor(props){
+    super(props)
+}
+
+    render(){
+        return <div className="base-container">
+        <div className="header">Login</div>
+        <div className="content">
+
+        </div>
+        </div>
+    }
+
+}
