34b76cb537b0e46e092f1261c828e328ebf17643
Prabhath Kotha <prabhath.k3@gmail.com>
2019-12-11 22:58:15 -0500

    Add username and authetication error handling

--- login.js
+++ login.js
@@ -7,14 +7,14 @@ $("#signup-button").click(function() {
             pass: $("#password").val()
         },
         success: function(data) {
-            console.log(data.status);
+            inject_signup_message();
         },
         error: function(xhr, status, error) {
-            //TODO let user know that username is taken
-            console.log(`Username : [${$("#username").val()}] already exists`)
+            inject_error_message(true);
         },
         async: false
     });
+    return false;
 });
 
 $("#login-button").click(function() {
@@ -29,9 +29,43 @@ $("#login-button").click(function() {
             console.log(data.jwt);
         },
         error: function(xhr, status, error) {
-            //TODO let user know that username is taken
-            console.log(`Incorrect info. username: [${$("#username").val()}] password: [${$("#password").val()}]`)
+            inject_error_message(false);
         },
         async: false
     });
+    return false;
 });
+
+function inject_error_message(new_user) {
+    $("#account-error").remove();
+    $("#account-signup").remove();
+    let html = `
+        <article id="account-error" class="message is-danger is-small">
+            <div class="message-header">
+                <p>Account error</p>
+            </div>
+            <div class="message-body">
+                ${new_user ? "Username already in use." : "Incorrect username or password."}
+            </div>
+        </article>
+    `;
+
+    $("#login-box").prepend(html);
+}
+
+function inject_signup_message() {
+    $("#account-error").remove();
+    $("#account-signup").remove();
+    let html = `
+        <article id="account-signup" class="message is-success is-small">
+            <div class="message-header">
+                <p>Success!</p>
+            </div>
+            <div class="message-body">
+                Account created. Try logging in.
+            </div>
+        </article>
+    `;
+
+    $("#login-box").prepend(html);
+}
